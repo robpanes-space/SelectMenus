483d4ea9236926e5553693d6bbd96f581477ab3b
rob panes <robpanes@icloud.com>
2024-07-09 11:39:07 +0800

    Update: select menus added cn function for merge tailwind class.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a dropdown select menu using tailwind and headless-ui.
 
 ## Available Scripts
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,33 +1,39 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...args) {
+  return twMerge(clsx(args));
+}
+
 /**
- * 
- * @param {*} value - check whether the value is empty | null | undefined 
+ *
+ * @param {*} value - check whether the value is empty | null | undefined
  * @returns boolean
  */
 export function isNullOrEmpty(value) {
+  if (value == null || value == undefined) {
+    return true;
+  }
 
-	if (value == null || value == undefined) {
-		return true;
-	}
+  if (typeof value == "string" && value.trim() == "") {
+    return true;
+  }
 
-	if (typeof value == 'string' && value.trim() == '') {
-		return true;
-	}
+  if (Array.isArray(value) && value.length === 0) {
+    return true;
+  }
 
-	if (Array.isArray(value) && value.length === 0) {
-		return true;
-	}
+  if (typeof value === "object" && Object.keys(value).length === 0) {
+    return true;
+  } else if (typeof value === "object" && Object.keys(value).length > 0) {
+    if (value.hasOwnProperty("error")) {
+      return true;
+    }
+  }
 
-	if (typeof value === 'object' && Object.keys(value).length === 0) {
-		return true;
-	} else if (typeof value === 'object' && Object.keys(value).length > 0) {
-		if (value.hasOwnProperty('error')) {
-			return true;
-		}
-	}
+  if (typeof value === "number" && isNaN(value)) {
+    return true;
+  }
 
-	if (typeof value === 'number' && isNaN(value)) {
-		return true;
-	}
-
-	return false;
+  return false;
 }
